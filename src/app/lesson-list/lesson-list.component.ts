a0f2b5710bc33c2b01ba045bf340d4e8999c1217
tomlaczik <tomlaczik@gmail.com>
2018-12-07 11:15:27 +0100

    Added support for taking and leaving lessons

--- src/app/lesson-list/lesson-list.component.ts
+++ src/app/lesson-list/lesson-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LessonService } from '../lesson.service';
+import { AuthService } from '../auth.service';
+import { Subject } from '../subject';
+import { Lesson } from '../lesson';
 
 @Component({
   selector: 'lesson-list',
@@ -8,29 +11,30 @@ import { LessonService } from '../lesson.service';
 })
 export class LessonListComponent implements OnInit {
 
-  private subjects = [
-    {name: "Tantárgy 1", lessons: [
-      {group: 6, registered: false},
-      {group: 2, registered: true},
-      {group: 8, registered: false},
-      {group: 11, registered: false},
-    ]},
-    {name: "Tantárgy 2", lessons: [
-      {group: 69, registered: false},
-      {group: 5, registered: false},
-      {group: 3, registered: true},
-    ]}
-  ];
+  private filteredSubjects: Subject[];
+  private subjects: Subject[];
 
-  private filteredSubjects;
-
-  constructor(private lessonService: LessonService) {}
+  constructor(private lessonService: LessonService, private authService: AuthService) {}
 
   async ngOnInit() {
-    this.filteredSubjects = await this.lessonService.getAll();
+    this.subjects = await this.lessonService.getAll();
+    this.filteredSubjects = this.subjects;
   }
 
   onFilterChange(data) {
+    console.log(this.authService.user);
+  }
+
+  isLessonActive(lesson: Lesson) {
+    return this.authService.user.activeLessons.some(aLesson => aLesson.id === lesson.id);
+  }
+
+  async takeLesson(lesson: Lesson) {
+    this.authService.user = await this.lessonService.addUser(lesson, this.authService.user);
+  }
+
+  async removeLesson(lesson: Lesson) {
+    this.authService.user = await this.lessonService.removeUser(lesson, this.authService.user);
   }
 
 }
--- src/app/lesson.service.ts
+++ src/app/lesson.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from './subject';
 import { httpOptions } from './auth.service';
+import { User } from './user';
+import { Lesson } from './lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,13 @@ export class LessonService {
 
   getAll(): Promise<Subject[]> {
     return this.http.get<Subject[]>(this.url, httpOptions).toPromise();
+  }
+
+  addUser(lesson: Lesson, user: User): Promise<User> {
+    return this.http.post<User>(`http://localhost:8080/users/${user.id}/activeLessons`, lesson, httpOptions).toPromise();
+  }
+
+  removeUser(lesson: Lesson, user: User): Promise<User> {
+    return this.http.delete<User>(`http://localhost:8080/users/${user.id}/activeLessons/${lesson.id}`, httpOptions).toPromise();
   }
 }
